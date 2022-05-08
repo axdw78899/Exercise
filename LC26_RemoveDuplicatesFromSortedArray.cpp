7282545346f4bbe5b4c5aa49e3b0d4dca5fd457d
axdw78899 <42911800+axdw78899@users.noreply.github.com>
2022-05-08 16:11:36 +0800

    Add files via upload

---
+++ LC26_RemoveDuplicatesFromSortedArray.cpp
@@ -0,0 +1,20 @@
+//LC26 Remove Duplicates from Sorted Array
+//[0,1,1,2,2] 5 ->[0,1,2] 3 
+int removeDuplicates(int* nums, int numsSize){
+	//建立新的計數器 
+    int count=0;//數有多少相同 
+    int i=0;
+    int newSize=numsSize; 
+    for(i=0;i<numsSize-1;i++){
+        if(nums[i]==nums[i+1]){ //相同,計數 
+            count=count+1;
+            newSize=newSize-1;
+        }
+        else	//不同,拉回去 
+        {
+            nums[i+1-count]=nums[i+1];
+        }
+        
+    }
+    return newSize;
+}
