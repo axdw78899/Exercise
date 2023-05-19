06d82452fd5e30e129818b945676e8a4784d1bd3
axdw78899 <42911800+axdw78899@users.noreply.github.com>
2023-05-19 08:43:59 +0800

    LC1_TwoSum.cpp

--- LC1_TwoSum.cpp
+++ LC1_TwoSum.cpp
@@ -1,23 +1,33 @@
 //LC1 two sum
-//µ¹¤@¦ê¼Æ¦r,²Õ¦X¥Xtarget
-//2¼hfor°j°é
+//çµ¦ä¸€ä¸²æ•¸å­—,çµ„åˆå‡ºtarget
+//2å±¤forè¿´åœˆ
 //(0,1),(0,2),(0,3),(0,4) i=0,j=1,2,3,4
 //      (1,2),(1,3),(1,4) i=1,j=  2,3,4 ...
 #include <stdio.h>
 #include <stdlib.h>
-int* twoSum(int* nums, int numsSize, int target, int* returnSize){
-    *returnSize = 2;
+
+int* twoSum(int nums[], int numsSize, int target) {
     int i = 0, j = 0;
     int n = numsSize;
     int* resultArr = (int*)malloc(sizeof(int) * 2);
-    for(i = 0; i < n-1; i++) {
-        for(j = i + 1; j < n; j++) {
-            if(target == nums[i] + nums[j]) {
+    for (i = 0; i < n - 1; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (target == nums[i] + nums[j]) {
                 resultArr[0] = i;
                 resultArr[1] = j;
-                return resultArr;
+                
             }
         }
     }
+
     return resultArr;
+}
+//æ¸¬è©¦
+int main() {
+    int arr[4] = {2,7,11,15};
+    int target = 9;
+
+    int *ans = twoSum(arr, 4, target);
+    printf("[%d %d] \n", ans[0],ans[1]);
+    return 0;
 }
